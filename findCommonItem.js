0813391d4a7adb0041f356fd5aab0e6c6935cc0b
Saravanaa Velpari <thisissaravanaa@gmail.com>
2020-10-04 21:35:49 +0530

    Create findCommonItem.js

---
+++ findCommonItem.js
@@ -0,0 +1,26 @@
+//Compare two arrays to find if it has common entry and return boolean.
+// Considering Time Complexity and to avoid Quadratic solution (nested for loop)
+
+array1 = [1,2,3,4,5,6] 
+array2 = [6,7,8,9,10]
+
+function compareForCommon(arr1,arr2){
+  let map={};
+  //converting first array to object
+  for(i=0;i<arr1.length;i++){
+    if(!map[arr1[i]]){
+      map[arr1[i]] = true; 
+    }
+  }
+  
+  for(i=0;i<arr2.length;i++){
+    if(map[arr2[i]]){
+      return true;
+    }
+  }
+  return false;
+}
+
+compareForCommon(array1,array2);
+
+//O(a + b)
